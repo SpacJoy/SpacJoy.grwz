7db301ecb3de146b3c8b26ab879e693e72de7eae
chen6019 <89252350+chen6019@users.noreply.github.com>
2025-09-02 00:16:41 +0800

    将缩进转换为制表符

--- main.js
+++ main.js
@@ -4,41 +4,41 @@ const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 // 创建窗口函数
 const createWindow = () => {
-    // 创建一个新的浏览器窗口
-    const win = new BrowserWindow({
-        width: 1980,
-        height: 1080,
-        webPreferences: {
-            // 在渲染器进程中预加载指定的脚本
-            preload: path.join(__dirname, 'preload.js'),
-            contextIsolation: false,
-            enableRemoteModule: true,
-            nodeIntegration: true,
-        }
-    })
+	// 创建一个新的浏览器窗口
+	const win = new BrowserWindow({
+		width: 1980,
+		height: 1080,
+		webPreferences: {
+			// 在渲染器进程中预加载指定的脚本
+			preload: path.join(__dirname, 'preload.js'),
+			contextIsolation: false,
+			enableRemoteModule: true,
+			nodeIntegration: true,
+		}
+	})
 
-    // 加载index.html文件到窗口中
-    win.loadFile('index.html')
+	// 加载index.html文件到窗口中
+	win.loadFile('index.html')
 }
 
 // 当应用准备就绪时执行回调函数
 app.whenReady().then(() => {
-    // 创建窗口
-    createWindow()
+	// 创建窗口
+	createWindow()
 
-    // 当应用被激活时执行回调函数
-    app.on('activate', () => {
-        // 如果没有窗口，则创建一个新窗口
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
+	// 当应用被激活时执行回调函数
+	app.on('activate', () => {
+		// 如果没有窗口，则创建一个新窗口
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 })
 
 // 当所有窗口都被关闭时执行回调函数
 app.on('window-all-closed', () => {
-    // 如果不是在Darwin系统上，则退出应用
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+	// 如果不是在Darwin系统上，则退出应用
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
--- preload.js
+++ preload.js
@@ -4,16 +4,16 @@ const { ipcRenderer } = require('electron');
 const axios = require('axios');
 
 window.electron = {
-  login: function(account, password) {
-    ipcRenderer.send('login', { account, password });
-  },
-  on: function(channel, func) {
-    ipcRenderer.on(channel, (event, ...args) => func(...args));
-  },
-  send: function (channel, data) {
-    ipcRenderer.send(channel, data);
-  },
-  axios: axios,
+	login: function(account, password) {
+		ipcRenderer.send('login', { account, password });
+	},
+	on: function(channel, func) {
+		ipcRenderer.on(channel, (event, ...args) => func(...args));
+	},
+	send: function (channel, data) {
+		ipcRenderer.send(channel, data);
+	},
+	axios: axios,
 };
 
 console.log('加载完成');
